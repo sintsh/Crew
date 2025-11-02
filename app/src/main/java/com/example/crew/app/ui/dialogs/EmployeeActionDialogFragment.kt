178d88de62fa11cdfb194e759305c200e76361d8
Amanuel Moha <mohaaman922@gmail.com>
2025-11-02 13:11:10 +0400

    basic dialog layout

--- app/src/main/java/com/example/crew/app/ui/dialogs/EmployeeActionDialogFragment.kt
+++ app/src/main/java/com/example/crew/app/ui/dialogs/EmployeeActionDialogFragment.kt
@@ -1,10 +1,12 @@
 package com.example.crew.app.ui.dialogs
 
+import android.content.res.Resources
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
 import androidx.fragment.app.DialogFragment
 import com.example.crew.R
 
@@ -17,6 +19,16 @@ class EmployeeActionDialogFragment : DialogFragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_employee_action_dialog, container, false)
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+        dialog?.window?.let { window ->
+            val width = (Resources.getSystem().displayMetrics.widthPixels * 0.80).toInt()
+            val height = WindowManager.LayoutParams.WRAP_CONTENT
+            window.setLayout(width, height)
+        }
     }
 
 }
